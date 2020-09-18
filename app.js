0bc14122b8c23683dd788a8eaf4011a32a4d36bf
unknown <you@example.com>
2020-09-18 15:08:14 +0200

    TP Syntaxe esnext Classe terminée

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let favoriteCityId = 'Rome';
 console.log(favoriteCityId);
 
-favoriteCityId ='Paris';
+favoriteCityId = 'Paris';
 console.log(favoriteCityId);
 
 const citiesId = ["paris", "nyc", "rome", "rio-de-janeiro"];
@@ -15,12 +15,74 @@ Commentaire dans la console = TypeError: Assignment to constant variable.
 citiesId.push("Tokyo");
 console.log(citiesId);
 
-function getWeather(cityId = 'Paris') {
-    let city = cityId;
-    let temperature = 20;
+//Création d’objet
 
-    return '{nom : ' + city + ' ' + 'temperature : ' + temperature + '}'; 
+function getWeather(cityId) {
+    let city = cityId.toUpperCase();
+    let temperature = 20;
+    return { city, temperature };
 }
 
 const weather = getWeather(favoriteCityId);
 console.log(weather);
+
+//Affectation destructurée
+
+const { city, temperature } = weather;
+
+console.log(city);
+console.log(temperature);
+
+//Rest operator
+
+let [parisId, nycID, ...otherCitiesID] = citiesId;
+
+console.log(parisId);
+console.log(nycID);
+console.log(otherCitiesID.length);
+
+//Classe
+
+class Trip {
+
+    constructor(id, name, imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.imageUrl = imageUrl;
+
+    }
+
+    toString() {
+        return 'Trip ' + '[ ' + this.id + ' , ' + this.name + ' , ' + this.imageUrl + ',' + this.price + ' ]';
+    }
+
+    get price() {
+        return this._price;
+    }
+    set price(newPrice) {
+        this._price = newPrice;
+    }
+
+    static getDefaultTrip(Trip){
+
+        Trip.id = 'rio-de-janeiro';
+        Trip.name = 'Rio de Janeiro';
+        Trip.imageUrl = 'img/rio-de-janeiro.jpg';
+        return Trip;
+    }
+}
+
+let parisTrip = new Trip('paris', 'Paris', 'img/paris.jpg');
+
+
+console.log(parisTrip);
+console.log(parisTrip.name);
+console.log(parisTrip.toString());
+
+parisTrip.price = 100;
+console.log(parisTrip.price);
+console.log(parisTrip.toString());
+
+let parisTrip2 = new Trip('paris', 'Paris', 'img/paris.jpg');
+const defaultTrip = Trip.getDefaultTrip(parisTrip2);
+console.log(defaultTrip.toString());
