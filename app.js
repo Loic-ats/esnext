e7d543ebb77b9c68d8c4de78fbf6c8554accf407
unknown <you@example.com>
2020-09-18 16:57:04 +0200

    TP Syntaxe esnext Promise, Set, Map, Arrow Function

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ class Trip {
     }
 
     toString() {
-        return 'Trip ' + '[ ' + this.id + ' , ' + this.name + ' , ' + this.imageUrl + ',' + this.price + ' ]';
+        return 'Trip ' + '[ ' + this.id + ' , ' + this.name + ' , ' + this.imageUrl + ' , ' + this.price + ' ]';
     }
 
     get price() {
@@ -63,7 +63,7 @@ class Trip {
         this._price = newPrice;
     }
 
-    static getDefaultTrip(Trip){
+    static getDefaultTrip(Trip) {
 
         Trip.id = 'rio-de-janeiro';
         Trip.name = 'Rio de Janeiro';
@@ -86,3 +86,71 @@ console.log(parisTrip.toString());
 let parisTrip2 = new Trip('paris', 'Paris', 'img/paris.jpg');
 const defaultTrip = Trip.getDefaultTrip(parisTrip2);
 console.log(defaultTrip.toString());
+
+//Héritage
+
+class FreeTrip extends Trip {
+
+    constructor(id, name, imageUrl) {
+        super(id, name, imageUrl);
+        this.price = 0;
+    }
+
+    toString() {
+        return 'Free' + super.toString();
+    }
+}
+
+let freeTrip = new FreeTrip('nantes', 'Nantes', 'img/nantes.jpg')
+console.log(freeTrip.toString());
+
+//Promise, Set, Map, Arrow Function
+
+class TripService {
+
+    constructor() {
+
+        this.trips = new set();
+
+        const trip1 = new Trip('paris', 'Paris', 'img/paris.jpg')
+        const trip2 = new Trip('nantes', 'Nantes', 'img/nantes.jpg')
+        const trip3 = new Trip('rio-de-janeiro', 'Rio de Janeiro', 'img/rio-de-janeiro.jpg')
+
+        this.trips.add(trip1);
+        this.trips.add(trip2);
+        this.trips.add(trip3);
+
+    }
+
+    findByName(tripName) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+
+                resolve(console.log(tripName));
+                reject(console.log("No trip with name xxx"));
+
+            }, 2000)
+        });
+    }
+}
+
+class PriceService {
+    constructor() {
+        const myMap = new Map();
+
+        myMap.set('paris', 100);
+        myMap.set('rio-de-janeiro', 800);
+        myMap.set('nantes', '');
+
+    }
+
+    findPriceByTripId(tripId) {
+        return new Promise((resolve, reject) => {
+
+            setTimeout(() => {
+                
+            }, 2000)
+        });
+    }
+}
+
